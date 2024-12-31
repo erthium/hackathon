e23d44e8381d361b7756413cbe542f41e5b4ea05
brkdnmz <brkdnmz99@gmail.com>
2024-12-31 20:14:51 +0300

    feat(backend): increase the rate limit to 40/sec

--- src/backend/app/dependencies/rate_limit.py
+++ src/backend/app/dependencies/rate_limit.py
@@ -1,4 +1,4 @@
 from fastapi import Depends
 from fastapi_limiter.depends import RateLimiter
 
-RateLimitDep = Depends(RateLimiter(times=5, seconds=1))
+RateLimitDep = Depends(RateLimiter(times=40, seconds=1))
